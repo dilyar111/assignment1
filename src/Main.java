910ec86c19b78bb08379d94dcfd4b7359765c328
dilyarabaizova <231274@astanait.edu.kz>
2023-12-24 20:45:19 +0600

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+import models.*;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner sc;
+
+    static {
+        try {
+            sc = new Scanner(new File("C:\\Users\\PC\\IdeaProjects\\Assignment1\\src\\source"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        while(sc.hasNextLine()){
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            shape.addPoint(new Point(x,y));
+        }
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongest());
+        System.out.println(shape.getAverageSide());
     }
 }
